2c4f0c74028bdceec4f7bbbdc12021bc7df96d81
Asadbek Shahobiddinov <148929482+Asadbek-1101@users.noreply.github.com>
2024-03-30 21:23:17 +0500

    New line

---
+++ Uyga_vazifa_2/main.py
@@ -0,0 +1,64 @@
+# 1 - misol
+
+# def teskari_tartib(listt):
+#     teskari_juft = []
+#     teskari_toq = []
+#     for i in listt:
+#         if i > 9 and i%2==0:
+#             teskari_juft.append(int(str(i)[::-1]))
+#         else:
+#             teskari_toq.append(int(str(i)))
+
+#     n = teskari_juft+teskari_toq
+#     m = len(teskari_juft+teskari_toq)    
+
+#     for el in range(m):
+#         for j in range(0, m-1):
+#             if n[j] > n[j+1]:
+#                 n[j], n[j+1] = n[j+1], n[j]
+#     return n
+
+
+# print(teskari_tartib([1,21,48,21,48,56,764]))
+#----------------------------------------------------------------------------
+
+
+
+#---------------------------------------------------------------------
+# 3 - masala
+
+# count = 0
+# file = open("txt.txt", "r")
+# reader = file.read()
+# for i in reader.split():
+#     i.isalpha()
+#     count += 1
+
+# print(count)
+# file.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
